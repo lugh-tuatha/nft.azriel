474b727b38712fe2fa18487474ca4ffb9a86bf08
Ace <aceonsteam001@gmail.com>
2023-02-14 17:33:06 +0800

    fix: conflict

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,9 @@ import reactLogo from './assets/svg/react.svg'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Button from 'react-bootstrap/Button';
-<<<<<<< HEAD
 
 import Btn  from './components/button';
 import Header from './components/header';
-=======
-import Btn  from './components/button'
-import Cards from './components/cards'
->>>>>>> 9ccdbcc73b320b78069ebdd39a78b35e0b9cf0c4
 
 import './App.css'
 
