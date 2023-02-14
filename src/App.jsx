ff911acb26e386aab51a50a72ed48ccf1e59c74d
Ace <aceonsteam001@gmail.com>
2023-02-14 17:50:05 +0800

    fix: mild error

--- src/App.jsx
+++ src/App.jsx
@@ -7,17 +7,16 @@ import Button from 'react-bootstrap/Button';
 
 import Btn  from './components/button';
 import Header from './components/header';
+import Cards from './components/cards';
 
 import './App.css'
 
-import Navbar from './components/navbar';
 
 function App() {
   return (
     <div className='app'>
       <Header />
       <Btn />
-      <Cards />
     </div>
   )
 }
