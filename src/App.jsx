cef7a104996e4762ce0a7219cc525928bfeaab5d
Ace <aceonsteam001@gmail.com>
2023-02-15 22:17:34 +0800

    remove: cards components

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,17 @@
 import { useState } from 'react'
 import $ from 'jquery';
-import reactLogo from './assets/svg/react.svg'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Button from 'react-bootstrap/Button';
 
-import Btn  from './components/button';
 import Header from './components/header';
-import Cards from './components/cards';
-import Feature from './components/feature';
-import TeamCards from './components/team-cards'
 
 import './App.css'
-
 
 function App() {
   return (
     <div className='app'>
       <Header />
-      <TeamCards />
     </div>
   )
 }
