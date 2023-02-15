43fe0401323c6047e82f72d9515d7c1c918d0686
Ace <aceonsteam001@gmail.com>
2023-02-15 10:19:24 +0800

    map: the gamesCard components

--- src/pages/GameZone.jsx
+++ src/pages/GameZone.jsx
@@ -1,17 +1,21 @@
-import GameCards from "../components/gameCards"
-
-import blackJackGame from '../assets/image/games/blackjack.png'
-
+import GameCards from "../components/gameCards";
+import blackJackGame from '../assets/image/games/blackjack.png';
 
 export default function GameZone() {
+  const games = [
+    {
+      title: "Blackjack Game",
+      description: "Blackjack is a card game where the objective is to beat the dealer by having a hand of cards that is worth more points than the dealer's hand, without going over 21.",
+      image: blackJackGame
+    }
+  ];
+
   return (
     <div>
       <p>Gamezone</p>
-      <GameCards 
-        title="Blackjack Game"
-        description="Blackjack is a card game where the objective is to beat the dealer by having a hand of cards that is worth more points than the dealer's hand, without going over 21."
-        image={blackJackGame}
-      />
+      {games.map(game => (
+        <GameCards title={game.title} description={game.description} image={game.image} />
+      ))}
     </div>
-  )
+  );
 }
