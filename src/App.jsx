08a6820909aa3d030b3db0a0dc6d370adf5e5754
Ace <aceonsteam001@gmail.com>
2023-02-16 11:17:36 +0800

    add: array components to marketplace

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,6 @@ function App() {
   return (
     <div className='app'>
       <Header />
-      <Cards />
       <TeamCards />
     </div>
   )
--- src/pages/Marketplace.jsx
+++ src/pages/Marketplace.jsx
@@ -1,7 +1,31 @@
+import Cards from "../components/cards";
+import nft1 from "../assets/image/nft-list/nft-1.png"
+import nft2 from "../assets/image/nft-list/nft-2.png"
+
+
 export default function Marketplace() {
+
+  const nfts = [
+    {
+      id: 1,
+      nftName: "Blackjack Game",
+      highestBid: "12 eth",
+      image: nft1
+    },
+    {
+      id: 2,
+      nftName: "Aceeee",
+      highestBid: "loremg over 21.",
+      image: nft2
+    }
+  ];
+
   return (
     <div>
       <p>Marketplace</p>
+      {nfts.map(nft => (
+        <Cards key={nft.id} nftName={nft.nftName} highestBid={nft.highestBid} image={nft.image} />
+      ))}
     </div>
-  )
+  );
 }
