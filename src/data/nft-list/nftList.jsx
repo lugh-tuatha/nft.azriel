31d332269f4beb729554e5eabc97da538af716be
Jommel Toledo <111914360+Hearts017@users.noreply.github.com>
2023-02-17 17:46:27 +0800

    add nft

--- src/data/nft-list/nftList.jsx
+++ src/data/nft-list/nftList.jsx
@@ -6,6 +6,53 @@ import kota from "../../assets/image/nft-list/kota.png"
 import supDuck from "../../assets/image/nft-list/SupDuck.png"
 import bean from "../../assets/image/nft-list/bean.png"
 import hooligan from "../../assets/image/nft-list/hooligan.png"
+import coolCats from "../../assets/image/nft-list/coolCats.avif"
+import doodle from "../../assets/image/nft-list/doodle.avif"
+import valhalla from "../../assets/image/nft-list/valhalla.avif"
+import rooTroop from "../../assets/image/nft-list/rooTroop.webp"
+import shrapnelOperatorsCollection from "../../assets/image/nft-list/shrapnelOperatorsCollection.avif"
+import strangersHQ from "../../assets/image/nft-list/strangersHQ.avif"
+import coolPets from "../../assets/image/nft-list/coolPets.avif"
+import akutars from "../../assets/image/nft-list/akutars.avif"
+import mutantApeYachtClub from "../../assets/image/nft-list/mutantApeYachtClub.avif"
+import azuki from "../../assets/image/nft-list/azuki.avif"
+import n1Force from "../../assets/image/nft-list/0n1Force.avif"
+import sappySeals from "../../assets/image/nft-list/sappySeals.avif"
+import repeatOffenders from "../../assets/image/nft-list/repeatOffenders.avif"
+import behindTheirEyes from "../../assets/image/nft-list/behindTheirEyes.avif"
+import blackIsBeautiful from "../../assets/image/nft-list/blackIsBeautiful.avif"
+import weirdNarrative from "../../assets/image/nft-list/weirdNarrative.avif"
+import parallelAlpha from "../../assets/image/nft-list/parallelAlpha.avif"
+import bridgeworldLegions from "../../assets/image/nft-list/bridgeworldLegions.avif"
+import timelessCharacters from "../../assets/image/nft-list/timelessCharacters.avif"
+import chimpers from "../../assets/image/nft-list/chimpers.avif"
+import goblintown from "../../assets/image/nft-list/goblintown.avif"
+import kpr from "../../assets/image/nft-list/kpr.avif"
+import lilPudgys from "../../assets/image/nft-list/lilPudgys.avif"
+import kaijuKingz from "../../assets/image/nft-list/kaijuKingz.avif"
+import acrocalypse from "../../assets/image/nft-list/acrocalypse.png"
+import cloneX from "../../assets/image/nft-list/cloneX.avif"
+import fallenRealms from "../../assets/image/nft-list/fallenRealms.avif"
+import allstarzOfficial from "../../assets/image/nft-list/allstarzOfficial.avif"
+import exoticHeroes from "../../assets/image/nft-list/exoticHeroes.avif"
+import homaGang from "../../assets/image/nft-list/homaGang.avif"
+import tubbyCats from "../../assets/image/nft-list/tubbyCats.avif"
+import littleTinasFantasy from "../../assets/image/nft-list/littleTinasFantasy.avif"
+import yaypegs from "../../assets/image/nft-list/yaypegs.avif"
+import beanbagFrens from "../../assets/image/nft-list/beanbagFrens.avif"
+import pudgyPenguins from "../../assets/image/nft-list/pudgyPenguins.avif"
+import renga from "../../assets/image/nft-list/renga.avif"
+import aotuNft from "../../assets/image/nft-list/aotuNft.avif"
+import alphasharks from "../../assets/image/nft-list/alphasharks.avif"
+import thePlagueNft from "../../assets/image/nft-list/thePlagueNft.avif"
+import rugRadio from "../../assets/image/nft-list/rugRadio.avif"
+import theOrdinalWar from "../../assets/image/nft-list/theOrdinalWar.avif"
+import qzukinft from "../../assets/image/nft-list/qzukinft.avif"
+import cryptoSimeji from "../../assets/image/nft-list/cryptoSimeji.avif"
+
+
+
+
 
 const nfts = [
   {
@@ -55,6 +102,264 @@ const nfts = [
     nftName: "Kota",
     price: 0.22 + " ETH",
     image: kota,
+  },
+  {
+    id: 9,
+    nftName: "Cool Cats",
+    price: 2.21 + " ETH",
+    image: coolCats,
+  },
+  {
+    id: 10,
+    nftName: "Doodle",
+    price: 6.5 + " ETH",
+    image: doodle,
+  },
+  {
+    id: 11,
+    nftName: "Valhalla",
+    price: 0.94 + " ETH",
+    image: valhalla,
+  },
+  {
+    id: 12,
+    nftName: "Roo Troop",
+    price: 0.244 + " ETH",
+    image: rooTroop,
+  },
+  {
+    id: 13,
+    nftName: "Shrapnel Operators Collection",
+    price: 0.078 + " ETH",
+    image: shrapnelOperatorsCollection,
+  },
+  {
+    id: 14,
+    nftName: "StrangersHQ",
+    price: 0.040 + " ETH",
+    image: strangersHQ,
+  },
+  {
+    id: 15,
+    nftName: "Cool Pets",
+    price: 0.119 + " ETH",
+    image: coolPets,
+  },
+  {
+    id: 16,
+    nftName: "Akutars",
+    price: 0.3027 + " ETH",
+    image: akutars,
+  },
+  {
+    id: 17,
+    nftName: "Mutant Ape Yacht Club",
+    price: 16.840 + " ETH",
+    image: mutantApeYachtClub,
+  },
+  {
+    id: 18,
+    nftName: "Azuki",
+    price: 16 + " ETH",
+    image: azuki,
+  },
+  {
+    id: 19,
+    nftName: "0N1 Force",
+    price: 1.7689 + " ETH",
+    image: n1Force,
+  },
+  {
+    id: 20,
+    nftName: "Sappy Seals",
+    price: 1.2 + " ETH",
+    image: sappySeals,
+  },
+  {
+    id: 21,
+    nftName: "Repeat Offenders",
+    price: 0.0199 + " ETH",
+    image: repeatOffenders,
+  },
+  {
+    id: 22,
+    nftName: "Behind Their Eyes",
+    price: 0.125 + " ETH",
+    image: behindTheirEyes,
+  },
+  {
+    id: 23,
+    nftName: "Black is Beautiful NFT",
+    price: 0.4 + " ETH",
+    image: blackIsBeautiful,
+  },
+  {
+    id: 24,
+    nftName: "Weird Narrative",
+    price: 0.5 + " ETH",
+    image: weirdNarrative,
+  },
+  {
+    id: 25,
+    nftName: "Parallel Alpha",
+    price: 0.0011 + " ETH",
+    image: parallelAlpha,
+  },
+  {
+    id: 26,
+    nftName: "Bridgeworld Legions",
+    price: 2.9 + " ETH",
+    image: bridgeworldLegions,
+  },
+  {
+    id: 27,
+    nftName: "Timeless Characters",
+    price: 0.390 + " ETH",
+    image: timelessCharacters,
+  },
+  {
+    id: 28,
+    nftName: "Chimpers",
+    price: 1.380 + " ETH",
+    image: chimpers,
+  },
+  {
+    id: 29,
+    nftName: "Goblintown",
+    price: 0.740 + " ETH",
+    image: goblintown,
+  },
+  {
+    id: 30,
+    nftName: "KPR",
+    price: 0.1625 + " ETH",
+    image: kpr,
+  },
+  {
+    id: 31,
+    nftName: "Lil Pudgys",
+    price: 0.577 + " ETH",
+    image: lilPudgys,
+  },
+  {
+    id: 32,
+    nftName: "Kaiju Kingz",
+    price: 0.247 + " ETH",
+    image: kaijuKingz,
+  },
+  {
+    id: 33,
+    nftName: "Acrocalypse",
+    price: 0.310 + " ETH",
+    image: acrocalypse,
+  },
+  {
+    id: 34,
+    nftName: "CloneX",
+    price: 5.6757 + " ETH",
+    image: cloneX,
+  },
+  {
+    id: 35,
+    nftName: "Fallen Realms",
+    price: 0.014 + " ETH",
+    image: fallenRealms,
+  },
+  {
+    id: 36,
+    nftName: "Allstarz Official",
+    price: 0.07 + " ETH",
+    image: allstarzOfficial,
+  },
+  {
+    id: 37,
+    nftName: "Exotic Heroes",
+    price: 0.055 + " ETH",
+    image: exoticHeroes,
+  },
+  {
+    id: 38,
+    nftName: "Homa Gang",
+    price: 0.1 + " ETH",
+    image: homaGang,
+  },
+  {
+    id: 39,
+    nftName: "Tubby Cats",
+    price: 0.16 + " ETH",
+    image: tubbyCats,
+  },
+  {
+    id: 40,
+    nftName: "Little Tinas Fantasy",
+    price: 0.0075 + " ETH",
+    image: littleTinasFantasy,
+  },
+  {
+    id: 41,
+    nftName: "Yaypegs",
+    price: 0.0398 + " ETH",
+    image: yaypegs,
+  },
+  {
+    id: 42,
+    nftName: "Beanbag Frens",
+    price: 0.115 + " ETH",
+    image: beanbagFrens,
+  },
+  {
+    id: 43,
+    nftName: "Pudgy Penguins",
+    price: 6.9 + " ETH",
+    image: pudgyPenguins,
+  },
+  {
+    id: 44,
+    nftName: "Renga",
+    price: 1.3622 + " ETH",
+    image: renga,
+  },
+  {
+    id: 45,
+    nftName: "AotuNFT",
+    price: 0.0588 + " ETH",
+    image: aotuNft,
+  },
+  {
+    id: 46,
+    nftName: "Alpha Sharks",
+    price: 0.5 + " ETH",
+    image: alphasharks,
+  },
+  {
+    id: 47,
+    nftName: "The Plague NFT",
+    price: 0.51 + " ETH",
+    image: thePlagueNft,
+  },
+  {
+    id: 48,
+    nftName: "Rug Radio Faces",
+    price: 0.069 + " ETH",
+    image: rugRadio,
+  },
+  {
+    id: 49,
+    nftName: "The Ordinal War",
+    price: 0.0449 + " ETH",
+    image: theOrdinalWar,
+  },
+  {
+    id: 50,
+    nftName: "Qzuki NFT",
+    price: 0.035 + " ETH",
+    image: qzukinft,
+  },
+  {
+    id: 51,
+    nftName: "Crypto Simeji",
+    price: 0.055 + " ETH",
+    image: cryptoSimeji,
   },
 ];
 
