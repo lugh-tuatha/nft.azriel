0c89110c9e829249629a69b9f1822608902a419c
Ace <aceonsteam001@gmail.com>
2023-02-18 01:20:40 +0800

    fix: bug

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,6 +1,8 @@
 import '../index.css'
 import Feature from '../components/feature'
 import TeamCards from '../components/team-cards'
+import Announcements from '../components/announcements'
+import Footer from '../components/footer'
 
 import './pages.css'
 
@@ -9,7 +11,8 @@ export default function Dashboard() {
     <div>
       <Feature />
       {/* <h1>Notable collection</h1> */}
-        
+      <Announcements />
+      <Footer />
       {/* <h1>Teams</h1> */}
     </div>
   )
