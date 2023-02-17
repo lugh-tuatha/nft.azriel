963811d202adc75139b6bf38b7c5bd9636b01159
Ace <aceonsteam001@gmail.com>
2023-02-17 23:14:43 +0800

    fix: margin in header

--- src/components/team-cards/index.jsx
+++ src/components/team-cards/index.jsx
@@ -6,17 +6,17 @@ function TeamCards() {
   return (
     <div className="teamCardsComponent">
       <div className="wrapper">
-      <div className="card">
-        <img src="./src/assets/image/frame/teamcards.jpg" />
-        <div className="info">
-          <h1>Ace</h1>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati
-            sed, inventore dignissimos ab quod iure.
-          </p>
+        <div className="card">
+          <img src="./src/assets/image/frame/teamcards.jpg" />
+          <div className="info">
+            <h1>Ace</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati
+              sed, inventore dignissimos ab quod iure.
+            </p>
+          </div>
         </div>
       </div>
-    </div>
     </div>
   )
 }
