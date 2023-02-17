4863e5f76d4c08b3f024ecde187c3fb5cf1f87bb
Ace <aceonsteam001@gmail.com>
2023-02-17 15:22:04 +0800

    fix: card components responsiveness

--- src/components/cards/index.jsx
+++ src/components/cards/index.jsx
@@ -6,27 +6,30 @@ import ethLogo from "../../assets/image/logo/eth-logo.png"
 function Cards({ nftName, price, image }) {
 
   return (
-    <div className="nftCard">
-      <div className="thumbnail">
-        <img src={image} alt="image of a cube" width="350px" height="auto" />
-      </div>
-      <div className='cardBody'>
-        <h1>{nftName}</h1>
-        <p>Our Equilibrium collection promotes balance and calm.</p>
-      </div>
-      <div className="details">
-        <div className="type">
-          <img className="icon" src={ethLogo} height="30px"/>
-          <p>{price}</p>
+    <div className="nftCardsContainer">
+      <div className="nftCard">
+        <div className="thumbnail">
+          <img src={image} alt="image of a cube" width="350px" height="auto" />
         </div>
-        <div className="time">
+        <div className='cardBody'>
+          <h1>{nftName}</h1>
+          <p>Our Equilibrium collection promotes balance and calm.</p>
+        </div>
+        <div className="details">
           <div className="type">
-            <img className="icon" src="https://assets.codepen.io/4479043/clock.png" height="30px"/>
-            <p>3 days left</p>
+            <img className="icon" src={ethLogo} height="30px"/>
+            <p>{price}</p>
+          </div>
+          <div className="time">
+            <div className="type">
+              <img className="icon" src="https://assets.codepen.io/4479043/clock.png" height="30px"/>
+              <p>3 days left</p>
+            </div>
           </div>
         </div>
       </div>
     </div>
+
   )
 }
 
