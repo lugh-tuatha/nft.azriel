7fe053afe91fe136ce79e2a2c6f2a345093dba26
Jommel Toledo <111914360+Hearts017@users.noreply.github.com>
2023-02-17 13:40:20 +0800

    add team components

--- src/components/team-cards/index.jsx
+++ src/components/team-cards/index.jsx
@@ -6,9 +6,18 @@ import './team-card.css'
 function TeamCards() {
   return (
     <div>
-      <div className=''>
-        
+      <div className="wrapper">
+      <div className="card">
+        <img src="./src/assets/image/frame/teamcards.jpg" />
+        <div className="info">
+          <h1>Ace</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati
+            sed, inventore dignissimos ab quod iure.
+          </p>
+        </div>
       </div>
+    </div>
     </div>
   )
 }
