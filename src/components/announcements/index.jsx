c7b3e1e2f69f0459f7ea9d12303d8443410bf87f
Ace <aceonsteam001@gmail.com>
2023-02-18 12:58:38 +0800

    small changes

--- src/components/announcements/index.jsx
+++ src/components/announcements/index.jsx
@@ -4,147 +4,37 @@ import img from '../../assets/image/dummy/good_apes__pfp_1668010066125.jpg';
 
 function Announcements() {
   return (
-    <div className='container'>
+    <div className='container-fluid announcement-container'>
       <div className="section-header">
         <h2 className="title">Trending announcements 📢 </h2>
         <span className="bandge">beta</span>
       </div>
-        <div className="card no-hover">
-            <ul className="list-group list-group-flush">
-                <li className="list-group-item">
-                  <div className="row">
-                    <div className="col-lg-3">
-                      <div className="profile">
-                        <img src={img} alt="" />
-                        <p className="name">Leprechauns</p>
-                      </div>
-                    </div>
-                    <div className="col-lg-6">
-                      <div className="topic">
-                        <p className="date">1 hour ago</p>
-                        <h6 className="title">We are thrilled to launch Golden Gambit!</h6>
-                      </div>
-                    </div>
-                    <div className="col-lg-3">
-                      <div className="more-details">
-                        <a href="" className='view-details'>View Details</a>
-                        <a href="" className='social'><i className="bi bi-twitter"></i></a>
-                      </div>
-                    </div>
-                  </div>
-                </li>
-                <li className="list-group-item">
-                  <div className="row">
-                    <div className="col-lg-3">
-                      <div className="profile">
-                        <img src={img} alt="" />
-                        <p className="name">Leprechauns</p>
-                      </div>
-                    </div>
-                    <div className="col-lg-6">
-                      <div className="topic">
-                        <p className="date">1 hour ago</p>
-                        <h6 className="title">We are thrilled to launch Golden Gambit!</h6>
-                      </div>
-                    </div>
-                    <div className="col-lg-3">
-                      <div className="more-details">
-                        <a href="" className='view-details'>View Details</a>
-                        <a href="" className='social'><i className="bi bi-twitter"></i></a>
-                      </div>
-                    </div>
-                  </div>
-                </li>
-                <li className="list-group-item">
-                  <div className="row">
-                    <div className="col-lg-3">
-                      <div className="profile">
-                        <img src={img} alt="" />
-                        <p className="name">Leprechauns</p>
-                      </div>
-                    </div>
-                    <div className="col-lg-6">
-                      <div className="topic">
-                        <p className="date">1 hour ago</p>
-                        <h6 className="title">We are thrilled to launch Golden Gambit!</h6>
-                      </div>
-                    </div>
-                    <div className="col-lg-3">
-                      <div className="more-details">
-                        <a href="" className='view-details'>View Details</a>
-                        <a href="" className='social'><i className="bi bi-twitter"></i></a>
-                      </div>
-                    </div>
-                  </div>
-                </li>
-                <li className="list-group-item">
-                  <div className="row">
-                    <div className="col-lg-3">
-                      <div className="profile">
-                        <img src={img} alt="" />
-                        <p className="name">Leprechauns</p>
-                      </div>
-                    </div>
-                    <div className="col-lg-6">
-                      <div className="topic">
-                        <p className="date">1 hour ago</p>
-                        <h6 className="title">We are thrilled to launch Golden Gambit!</h6>
-                      </div>
-                    </div>
-                    <div className="col-lg-3">
-                      <div className="more-details">
-                        <a href="" className='view-details'>View Details</a>
-                        <a href="" className='social'><i className="bi bi-twitter"></i></a>
-                      </div>
-                    </div>
-                  </div>
-                </li>
-                <li className="list-group-item">
-                  <div className="row">
-                    <div className="col-lg-3">
-                      <div className="profile">
-                        <img src={img} alt="" />
-                        <p className="name">Leprechauns</p>
-                      </div>
-                    </div>
-                    <div className="col-lg-6">
-                      <div className="topic">
-                        <p className="date">1 hour ago</p>
-                        <h6 className="title">We are thrilled to launch Golden Gambit!</h6>
-                      </div>
-                    </div>
-                    <div className="col-lg-3">
-                      <div className="more-details">
-                        <a href="" className='view-details'>View Details</a>
-                        <a href="" className='social'><i className="bi bi-twitter"></i></a>
-                      </div>
-                    </div>
-                  </div>
-                </li>
-                <li className="list-group-item">
-                  <div className="row">
-                    <div className="col-lg-3">
-                      <div className="profile">
-                        <img src={img} alt="" />
-                        <p className="name">Leprechauns</p>
-                      </div>
-                    </div>
-                    <div className="col-lg-6">
-                      <div className="topic">
-                        <p className="date">1 hour ago</p>
-                        <h6 className="title">We are thrilled to launch Golden Gambit!</h6>
-                      </div>
-                    </div>
-                    <div className="col-lg-3">
-                      <div className="more-details">
-                        <a href="" className='view-details'>View Details</a>
-                        <a href="" className='social'><i className="bi bi-twitter"></i></a>
-                      </div>
-                    </div>
-                  </div>
-                </li>
-            </ul>
-        </div>
+      <div className="card no-hover">
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item">
+            <div className="row">
+              <div className="col-lg-3">
+                <div className="profile">
+                  <img src={img} alt="" />
+                  <p className="name">Leprechauns</p>
+                </div>
+              </div>
+              <div className="col-lg-6">
+                <div className="topic">
+                  <p className="date">1 hour ago</p>
+                  <h6 className="title">We are thrilled to launch Golden Gambit!</h6>
+                </div>
+              </div>
+              <div className="col-lg-3">
+                <div className="more-details">
+                  <a href="" className='view-details'>View Details</a>
+                  <a href="" className='social'><i className="bi bi-twitter"></i></a>
+                </div>
+              </div>
+            </div>
+          </li>
+        </ul>
+      </div>
     </div>
   )
 }
--- src/components/collections/index.jsx
+++ src/components/collections/index.jsx
@@ -5,17 +5,17 @@ function Collection({ image, title, floorPrice, volume }) {
     <div className="card">
       <img src={image} className="card-img-top img-fluid" alt="..." />
       <div className="card-body">
-          <h5 className="card-title">{title}</h5>
-          <div className="row info">
-            <div className="col">
-              <p className="label">floor</p>
-              <p className="value">{floorPrice}</p>
-            </div>
-            <div className="col">
-              <p className="label">Total volume</p>
-              <p className="value">{volume}</p>
-            </div>
+        <h5 className="card-title">{title}</h5>
+        <div className="row info">
+          <div className="col">
+            <p className="label">floor</p>
+            <p className="value">{floorPrice}</p>
           </div>
+          <div className="col">
+            <p className="label">Total volume</p>
+            <p className="value">{volume}</p>
+          </div>
+        </div>
       </div>
     </div>
   )
